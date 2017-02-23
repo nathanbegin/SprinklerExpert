c8b6574ec5317d8aa48eb0235103d0e97595d5f5
Nathan Begin <nathanbegin@gmail.com>
2017-02-22 22:15:06 -0500

    Initialization of the project

---
+++ SE_main_code.py
@@ -0,0 +1,12 @@
+####################################################
+//  Created by Nathan Begin on 22-02-2017
+//  Créé par Nathan Begin le 22-02-2017
+//  nathanbegin@gmail.com
+//  Copyright 2017 ©  All rights reserved/.
+
+####################################################
+
+#Import the necessary libraries
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
